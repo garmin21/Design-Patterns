e2fea1653c4ae576555b0403a4a9625b7ad225a7
LJ <2180727350@qq.com>
2021-03-11 00:41:05 +0800

    改成私有仓库

---
+++ 01-path/01-path.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+// 获取路径下的文件名
+// console.log(path.basename('C:\\temp\\name.html')); // name.html
+
+// 参数二，是去除后缀
+// console.log(path.basename('C:\\temp\\name.html', '.html')); // name
+
+
+
+// 获取所有的进程
+// console.log(process.env.PATH); 
+// 将所有进程序列化成一个数组
+// console.log(process.env.PATH.split(path.delimiter));
+
+
+
+
